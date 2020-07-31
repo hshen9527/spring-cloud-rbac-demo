1a80be232b827c9ea09400edcad61c00d77ea5cb
WLX <2391255979@qq.com>
2020-07-31 17:46:42 +0800

    做出了修改

--- rbac/src/views/RoleManage.vue
+++ rbac/src/views/RoleManage.vue
@@ -11,17 +11,36 @@
         </el-menu>
 
         <!-- 侧边栏 -->
-        <el-menu :default-active="defaultActive"
-            router
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            :collapse="isCollapse">
-            <template v-for="tag in tags">
-                <template v-for="item in tag.item" >
-                    <!-- 侧边栏 -->
-                </template>
-            </template>
-        </el-menu>
+        <el-row class="tac">
+        <!-- 侧边栏宽度 -->
+        <el-col :span="4"> 
+            <el-menu
+                default-active="1"
+                class="el-menu-vertical-demo"
+                unique-opened 
+                router>
+                <!-- tag -->
+                <el-submenu v-for="tag in tags" :key="tag">
+                    <template slot="title" class="tag">
+                        <i class="el-icon-message"></i>
+                        <span>{{tag.name}}</span>
+                    </template>
+                    <!-- item -->
+                    <el-menu-item-group v-for="item in tag.value" :key="item">
+                        <template slot="title"></template>
+                        <el-menu-item >{{item.name}}</el-menu-item>
+                     </el-menu-item-group>
+                </el-submenu>
+            </el-menu>
+        </el-col>
+        </el-row>
+
+        <!-- 右侧显示 -->
+        <el-col :span="24" class="content-wrapper">
+            <transition name="fade" mode="out-in">
+                <router-view></router-view>
+            </transition>
+        </el-col>
     </div>
 </template>
 
@@ -31,26 +50,37 @@ export default {
     data() {
         return{
             tags:[
-                [{name:"tag1"},
-                 {name:"tag2"},
-                 {name:"tag3"}],
-                [{name:"tag1"},
-                 {name:"tag2"},
-                 {name:"tag3"}],
-                [{name:"tag1"},
-                 {name:"tag2"},
-                 {name:"tag3"}],
-                [{name:"tag1"},
-                 {name:"tag2"},
-                 {name:"tag3"}],
-                [{name:"tag1"},
-                 {name:"tag2"},
-                 {name:"tag3"}]
+                {
+                    name: "tag1",
+                    value: [
+                        {name: "item1"},
+                        {name: "item2"},
+                        {name: "item3"}
+                    ]
+                },
+                {
+                    name: "tag1",
+                    value: [
+                        {name: "item1"},
+                        {name: "item2"},
+                        {name: "item3"}
+                    ]
+                },
+                {
+                    name: "tag1",
+                    value: [
+                        {name: "item1"},
+                        {name: "item2"},
+                        {name: "item3"}
+                    ]
+                },
             ]}
     }
 }
 </script>
 
 <style>
-
+.tag{
+    color: lightBlue;
+}
 </style>
